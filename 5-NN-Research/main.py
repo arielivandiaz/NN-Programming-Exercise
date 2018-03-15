5c2e3ee54f75f09373cfce5b3d708ac7a2fa0728
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-03-15 12:37:48 -0300

    first test

--- 5-NN-Research/main.py
+++ 5-NN-Research/main.py
@@ -145,7 +145,7 @@ def get_data (param):
 	# flatten 28*28 images to a 784 vector for each image
 	num_pixels = X_train.shape[1] * X_train.shape[2]
 
-	if(params=='simple_NN'):
+	if(param =='simple_NN'):
 		X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
 		X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 	else:
