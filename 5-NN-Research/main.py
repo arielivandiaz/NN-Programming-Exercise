f94a650d4845f22983a7a3718a03cfb7a65afdca
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-03-15 15:58:23 +0100

    testing

--- 5-NN-Research/main.py
+++ 5-NN-Research/main.py
@@ -3,7 +3,13 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
+from keras import backend 
+backend.set_image_dim_ordering('th')
+
 
 from keras import backend
 
