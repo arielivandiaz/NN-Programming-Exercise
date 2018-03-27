949bbab9b46064fba2bb22d39b238da675564188
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-03-27 13:18:39 +0200

    3.1

--- 5-NN-Research/main.py
+++ 5-NN-Research/main.py
@@ -300,9 +300,19 @@ def test1_NN (params,X_train, y_train, X_test, y_test, num_pixels , num_classes)
 
 	for i in activations:
 		params.activation_1=i
-		for j in activations:				
-			params.activation_2=j
-			model = simple_NN(params)
+		for j in activations:
+			if(params.neural_network=='simple_NN'):
+				params.activation_2=j
+				model = simple_NN(params)
+				
+			elif (params.neural_network=='simple_CNN'):
+				params.activation_3=j
+				model = simple_CNN(params)
+
+			elif (params.neural_network=='large_CNN'):
+				params.activation_5=j
+				model = large_CNN(params)			
+	
 			run_evaluation(model, X_train, y_train, X_test, y_test,params)
 			backend.clear_session()
 
@@ -420,11 +430,12 @@ if __name__ == '__main__':
 	print (params.b_size)
 	print ("\n")
 	"""
+	test1_NN (params,X_train, y_train, X_test, y_test, num_pixels , num_classes)
 	#test4_NN(params,X_train, y_train, X_test, y_test, num_pixels , num_classes)
 
 	#test5_NN(params,X_train, y_train, X_test, y_test, num_pixels , num_classes)
 
-	
+	"""
 	# Build the model
 	if(params.neural_network=='simple_NN'):
 		model = simple_NN(params)
@@ -437,7 +448,7 @@ if __name__ == '__main__':
 
 	# Fit the model, run evaluation and get output file
 	run_evaluation(model, X_train, y_train, X_test, y_test,params)
-
+	"""
 	# Clean up
 	backend.clear_session()
 	
