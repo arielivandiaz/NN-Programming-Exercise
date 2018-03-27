77700f501969ac0c3fff476abe2d7d937921c474
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-03-27 15:58:04 +0200

    single core

--- 5-NN-Research/main.py
+++ 5-NN-Research/main.py
@@ -10,7 +10,7 @@ from keras.utils import np_utils
 from keras import backend 
 backend.set_image_dim_ordering('th')
 #For run in single core
-backend.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_‌​parallelism_threads=‌​32, inter_op_parallelism_threads=32)))
+backend.set_session(backend.tf.Session(config=backend.tf.ConfigProto(intra_op_‌​parallelism_threads=‌​32, inter_op_parallelism_threads=32)))
 
 from keras import backend
 
