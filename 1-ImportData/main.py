7739197d282e0ab6e47aaa18caede8d1ec94f269
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-03-29 15:53:00 +0200

    import data new version

--- 1-ImportData/main.py
+++ 1-ImportData/main.py
@@ -4,14 +4,35 @@ import matplotlib.pyplot as plt
 import random
 # load (downloaded if needed) the MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# plot 4 images as gray scale
-plt.subplot(221)
+# plot some images as gray scale
+
+plt.subplot(331)
+plt.axis('off')
 plt.imshow(X_train[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
+plt.subplot(332)
+plt.axis('off')
 plt.imshow(X_test[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
+plt.subplot(333)
+plt.axis('off')
 plt.imshow(X_train[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
+plt.subplot(334)
+plt.axis('off')
 plt.imshow(X_test[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+plt.subplot(335)
+plt.axis('off')
+plt.imshow(X_train[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+plt.subplot(336)
+plt.axis('off')
+plt.imshow(X_test[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+plt.subplot(337)
+plt.axis('off')
+plt.imshow(X_train[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+plt.subplot(338)
+plt.axis('off')
+plt.imshow(X_test[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+plt.subplot(339)
+plt.axis('off')
+plt.imshow(X_train[random.randint(0, len(X_test))], cmap=plt.get_cmap('gray'))
+
 # show the plot
 plt.show()
